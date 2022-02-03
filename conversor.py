5daf2022e07b1db05e9e6b99fb4927bbb9503cd5
Hernan <hernan@mukiku.com>
2022-02-03 19:31:41 -0300

    'add-if-sentence-for-select-pesos-of-other-countries'

--- conversor.py
+++ conversor.py
@@ -1,10 +1,33 @@
+menu = """
+Bienvenido al conversor de monedas 🪙
+
+1 - Pesos argentinos
+2 - Pesos mexicanos
+
+Elige una opción: """
+opcion = input(menu)
+
+if opcion == '1':
+    # valor ---- 1 dls
+    valor_dolar = input("Ingresa el valor del dolar actual en tu moneda: ")
+    valor_dolar = float(valor_dolar)
+    # valor ---- x dls
+    valor_pesos = input(
+        "Ingresa el valor en pesos argentinos que queres convertir: ")
+    valor_pesos = float(valor_pesos)
+    resultado = int(valor_pesos / valor_dolar)
+    res = round(resultado, 2)
+    print("El resultado es: U$S", res)
+else:
+    # valor ---- 1 dls
+    valor_dolar = input("Ingresa el valor del dolar actual en tu moneda: ")
+    valor_dolar = float(valor_dolar)
+    # valor ---- x dls
+    valor_pesos = input(
+        "Ingresa el valor en pesos mexicanos que queres convertir: ")
+    valor_pesos = float(valor_pesos)
+    resultado = int(valor_pesos / valor_dolar)
+    res = round(resultado, 2)
+    print("El resultado es: U$S", res)
 # No puedo utilizar funciones, imports o clases por el momento
 # conversor de monedas
-valor_dolar = input("Ingresa el valor del dolar actual: ")  # valor ---- 1 dls
-valor_dolar = float(valor_dolar)
-# valor ---- x dls
-valor_pesos = input("Ingresa el valor en pesos que queres convertir: ")
-valor_pesos = float(valor_pesos)
-resultado = int(valor_pesos / valor_dolar)
-res = round(resultado, 2)
-print("El resultado es: U$S", res)
